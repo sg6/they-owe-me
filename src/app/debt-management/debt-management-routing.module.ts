76700f69519bbcfeb9a5c7e24e9b88d7029cb169
Jakob Gruber <e0926280@student.tuwien.ac.at>
2018-03-25 16:14:11 +0200

    added router-link to person-details

--- src/app/debt-management/debt-management-routing.module.ts
+++ src/app/debt-management/debt-management-routing.module.ts
@@ -2,10 +2,12 @@ import {RouterModule, Routes} from '@angular/router';
 import {NgModule} from '@angular/core';
 import {PersonListComponent} from './components/persons/container/person-list.component';
 import {PersonEditComponent} from './components/persons/detail/person-edit.component';
+import {PersonDetailComponent} from './components/persons/detail/person-detail.component';
 
 const routes: Routes = [
   {path: '', component: PersonListComponent},
-  {path: 'edit/:id', component: PersonEditComponent}
+  {path: 'edit/:id', component: PersonEditComponent},
+  {path: 'debt-management/detail/:id', component: PersonDetailComponent}
 ];
 
 @NgModule({
